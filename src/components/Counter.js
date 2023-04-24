e8372be1d19cf8c331a19e76527df2a1cf990527
Spaun1 <andreispaun7@gmail.com>
2023-04-24 10:55:24 +0300

    example connect react and redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,7 @@
+import { connect } from 'react-redux';
+
+import { inc, dec, rnd } from '../actions';
+
 const Counter = ({ counter, dec, inc, rnd }) => {
   return (
     <div>
@@ -15,4 +19,25 @@ const Counter = ({ counter, dec, inc, rnd }) => {
   );
 };
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    counter: state.value,
+  };
+};
+
+// mapDispatchToProps?: Function | Object - Пример с функцией и объектом.
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     inc: () => dispatch(inc()),
+//     dec: () => dispatch(dec()),
+//     rnd: () => {
+//       const value = Math.floor(Math.random() * 10);
+//       dispatch(rnd(value));
+//     },
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+export default connect(mapStateToProps, { inc, dec, rnd })(Counter);
