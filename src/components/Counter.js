b423500f8e20fd2726ff47f36461d005437cdebd
Spaun1 <andreispaun7@gmail.com>
2023-04-25 13:33:11 +0300

    refactor: use react and redux with hooks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,43 +1,25 @@
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { inc, dec, rnd } from '../actions';
 
-const Counter = ({ counter, dec, inc, rnd }) => {
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1 className="jumbotron">{counter}</h1>
-      <button onClick={dec} className="btn btn-primary">
-        DEC
-      </button>
-      <button onClick={inc} className="btn btn-primary">
+      <button onClick={() => dispatch(inc())} className="btn btn-primary">
         INC
       </button>
-      <button onClick={rnd} className="btn btn-primary">
+      <button onClick={() => dispatch(dec())} className="btn btn-primary">
+        DEC
+      </button>
+      <button onClick={() => dispatch(rnd())} className="btn btn-primary">
         RND
       </button>
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    counter: state.value,
-  };
-};
-
-// mapDispatchToProps?: Function | Object - Пример с функцией и объектом.
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     inc: () => dispatch(inc()),
-//     dec: () => dispatch(dec()),
-//     rnd: () => {
-//       const value = Math.floor(Math.random() * 10);
-//       dispatch(rnd(value));
-//     },
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
-
-export default connect(mapStateToProps, { inc, dec, rnd })(Counter);
+export default Counter;
--- src/reducer.js
+++ src/reducer.js
@@ -1,27 +1,23 @@
 const initialState = {
-  value: 0,
+  counter: 0,
 };
 
-// функция reducer всегда должна быть чистой функцией-(должна зависить от state который в нее приходит и от action), при этом она должна возвращать один и тот же результат при одинаковых аргументах и не иметь никаких побочных эффектов.
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INC':
-      // return state + 1; (ниже мы соблюдаем принципы иммутабельности).
       return {
         ...state,
-        value: state.value + 1,
+        counter: state.counter + 1,
       };
     case 'DEC':
-      // return state - 1; (ниже мы соблюдаем принципы иммутабельности).
       return {
         ...state,
-        value: state.value - 1,
+        counter: state.counter - 1,
       };
     case 'RND':
-      // return state * action.payload; (ниже мы соблюдаем принципы иммутабельности).
       return {
         ...state,
-        value: state.value * action.payload,
+        counter: state.counter * action.payload,
       };
     default:
       return state;
